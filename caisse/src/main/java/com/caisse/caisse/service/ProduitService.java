03b7c64f160ba7ea0bf3c9d5a9659bfe10ad2351
fedi <nefzi.fedi7@gmail.com>
2022-06-28 23:39:38 +0100

    firsy commit

---
+++ caisse/src/main/java/com/caisse/caisse/service/ProduitService.java
@@ -0,0 +1,58 @@
+package com.caisse.caisse.service;
+
+import com.caisse.caisse.exception.ProduitNotFoundException;
+import com.caisse.caisse.model.Produit;
+import com.caisse.caisse.repo.ProduitRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ProduitService {
+    private  final ProduitRepo produitRepo;
+
+    @Autowired
+    public ProduitService(ProduitRepo produitRepo) {
+        this.produitRepo = produitRepo;
+    }
+
+
+    public Produit addProduit(Produit produit) {
+        produit.setProduitCode(UUID.randomUUID().toString());
+        return this.produitRepo.save(produit);
+    }
+
+
+    public List<Produit> findAllProduit(){
+        return produitRepo.findAll();
+    }
+
+    public Produit updateProduit(Produit produit){
+        return produitRepo.save(produit);
+    }
+
+    public Produit findProduitByID(Long id) {
+        return produitRepo.findProduitById(id)
+                .orElseThrow(() -> new ProduitNotFoundException("produit by id" + id + "was not found"));
+    }
+
+
+    public void deleteProduit(Long id){
+        boolean exists=produitRepo.existsById(id);
+        if (!exists){
+            throw  new IllegalStateException(
+                    "produit with id"+id+"does not exists");
+
+
+        }
+        produitRepo.deleteProduitById(id);
+
+    }
+
+
+
+
+
+}
